1dcf5cdb4d0e89dc88e9e0cc6b699d64585751ef
EthWiz <82321943+EthWiz@users.noreply.github.com>
2023-07-18 18:59:37 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ function App() {
     <>
       <h1>Hello World!</h1>
       <h2>Added this line using WF - changed again</h2>
-      <h3>and again</h3>
-      <h4>and again - ad matai!!!!</h4>
     </>
   );
 }
