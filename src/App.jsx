7913c0c05bee5b0261ae722cc04009e899b02984
amitai ruskin <ami.ruskin@gmail.com>
2023-07-19 14:13:32 +0300

    fghdf

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,7 @@ function App() {
   return (
     <>
       <h1>Hello World!</h1>
-      <h2>Added this line using WF - changed again</h2>
-      <h3>Changed priv key in anoying yml</h3>
-      <h4>Fixed user name</h4>
-      <h5>Generated new key</h5>
+      <p>In construction, prepare for somthing great!!</p>
     </>
   );
 }
